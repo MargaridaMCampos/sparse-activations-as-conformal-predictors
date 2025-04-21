49eef5bab86352eaf6f14f829a51f9721508c434
MargaridaMCampos <53880552+MargaridaMCampos@users.noreply.github.com>
2025-04-21 14:03:23 +0000

    updating

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 import math
+import numpy as np
 from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='Sparse Activations as Conformal Predictors',
+    page_icon=':material/join_inner:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
@@ -64,88 +65,34 @@ gdp_df = get_gdp_data()
 
 # Set the title that appears at the top of the page.
 '''
-# :earth_americas: GDP dashboard
+# :material/join_inner: Sparse Activations as Conformal Predictors
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
+
 '''
 
 # Add some spacing
 ''
 ''
+''' ## Theoretical Setup
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+'''
+tab1, tab2, tab3 = st.tabs(["📊 Conformal Prediction", "📈 Sparse Activations","🔗 Equivalence"])
+data = np.random.randn(10, 1)
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+tab1.subheader("A tab with a chart")
+tab1.line_chart(data)
 
-countries = gdp_df['Country Code'].unique()
+tab2.subheader("Sparse Alternatives to Softmax")
+tab2.write('''
+           In a classification task with $K$ classes, 
+           typical predictive models output a vector of label scores $\mathbf{z}\in \mathbb{R}^K$,
+           which is converted to a probability vector through some transformation - typically *softmax*.
+           
+           [Martins and Astudillo (2016)](https://arxiv.org/pdf/1602.02068) introduced *sparsemax* -
+           an alternative transformation capable of producing sparse outputs.
+           [Peters et al. (2019)](https://arxiv.org/pdf/1905.05702) showed that both transformations
+           are particular cases of the $\gamma\mathrm{-entmax}$ family
+           ''')
 
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+tab3.subheader("A tab with the data")
+tab3.write(data)
